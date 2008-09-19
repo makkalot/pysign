6fb9799134e2fcb61fc50dad46ef0a48979ec9a8
makkalot <makkalot@gmail.com>
2008-09-20 00:44:54 +0300

    store the constants here for now ,may use a conf file under /etc later if needed

---
+++ config.py
@@ -0,0 +1,6 @@
+#Some constants will be stored here 
+MY_STORE = "/home/makkalot/mygits/pysign/imzaci/chain"
+SSL_EXECUTABLE = "openssl"
+SSL_CONF = "openssl.cnf"
+TEMPLATE_CNF = "template.cnf"
+INTERNAL_DB_FILE="internal.db"
