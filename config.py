b0d0320c059585ecb746f63e4eed3e521ebb3958
makkalot <makkalot@acerfedora.(none)>
2008-10-05 14:20:57 +0300

    i preinstalled my comp :)

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #Some constants will be stored here 
-MY_STORE = "/home/makkalot/mygits/pysign/imzaci/chain"
+MY_STORE = "/home/makkalot/code_repo/my_git/pysign/imzaci/chain"
 SSL_EXECUTABLE = "openssl"
 SSL_CONF = "openssl.cnf"
 TEMPLATE_CNF = "template.cnf"
