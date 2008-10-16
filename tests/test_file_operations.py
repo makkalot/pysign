6a1bf41a97fca45a0e60b0e7b619a7485699e51e
makkalot <makkalot@acerfedora.(none)>
2008-10-16 14:56:53 +0300

    some test changes not big deal

--- tests/test_file_operations.py
+++ tests/test_file_operations.py
@@ -2,8 +2,8 @@
 from imzaci.digest.file_operations import FileList
 from imzaci.digest.digest_util import DigestUtil
 from imzaci.digest.file_operations import DirHashHandler
-#TEST_DIR = "/home/makkalot/code_repo/my_git/pysign/imzaci/chain"
-TEST_DIR = "/media/disk"
+TEST_DIR = "/home/makkalot/code_repo/my_git/pysign/imzaci/chain"
+#TEST_DIR = "/media/disk"
 def compute_hash_without_thread(base_dir):
     """
     Because the thread based implementation alway scares me
@@ -30,7 +30,7 @@ def test_dir_hash_creator():
     """
     straight = compute_hash_without_thread(TEST_DIR)
     #print straight
-    for i in range(0,1):
+    for i in range(0,1000):
         dh = DirHashHandler(TEST_DIR)
         threaded = dh.get_final_hash_dict()
         #print threaded
