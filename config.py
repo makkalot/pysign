253d2aaef3f9163839ec598ee65ed3890416c142
makkalot <makkalot@acerfedora.(none)>
2008-12-12 21:58:53 +0200

    lets move those globals

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ TEMPLATE_CNF = "template.cnf"
 INTERNAL_DB_FILE="internal.db"
 TRUSTED_DB_PATH = "/home/makkalot/code_repo/my_git/pysign/imzaci/chain/trusted"
 UNTRUSTED_DB_PATH = "/home/makkalot/code_repo/my_git/pysign/imzaci/chain/untrusted"
+#here you put the certs you trust
+TRUSTED_DB = "trusted"
+#here you put the banned certs and theirs CA's
+UNTRUSTED_DB = "untrusted"
+#here you put your stuff !
+MY_STORE= "mystore"
+
+
